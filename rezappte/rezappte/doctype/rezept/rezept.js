0dae77a9abce87995deb3928ac2817075fea6aad
Ivanios <ivan.lochbihler@libracore.com>
2023-11-11 11:10:58 +0100

    zs

--- rezappte/rezappte/doctype/rezept/rezept.js
+++ rezappte/rezappte/doctype/rezept/rezept.js
@@ -2,7 +2,37 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Rezept', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+		calculate_ingredients(frm);
+    },
+    origin: function(frm) {
+        autocomplete_region(frm);
+    }
 });
+
+function autocomplete_region() {
+	let asiatisch = ["Thailand", "Vietnam", "China", "Japan"];
+	let europäisch = ["Italien", "Frankreich", "Österreich", "Schweiz", "Deutschland"];
+    if (asiatisch.includes(cur_frm.doc.origin)) {
+        cur_frm.set_value("region", "Asiatisch");
+    } else if (europäisch.includes(cur_frm.doc.origin)) {
+		cur_frm.set_value("region", "Europäisch");
+    }
+}
+
+function calculate_ingredients(frm) {
+	frappe.call({
+        'method': 'rezappte.rezappte.doctype.rezept.rezept.calculate_ingredients',
+        'args': {
+            'ingredients': frm.doc.ingredients
+        },
+        'callback': function(r) {
+			console.log(r.message)
+            cur_frm.set_value("ingredients_qty", r.message);
+        }
+    });
+}
+
+//~ function calculate_ingredients(frm) {
+	//~ cur_frm.set_value("ingredients_qty", frm.doc.ingredients.length);
+//~ }
--- rezappte/rezappte/doctype/rezept/rezept.py
+++ rezappte/rezappte/doctype/rezept/rezept.py
@@ -3,8 +3,49 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Rezept(Document):
 	pass
+
+@frappe.whitelist()
+def calculate_ingredients(ingredients):
+	ing = """
+	SELECT `name` 
+	FROM `tabRezept Zutaten`
+	WHERE `alternative` = 0;"""
+	real_ingredients = []
+	for amount in ingredients:
+		# ~ frappe.log_error>(amount['alternative'], "amount")
+		frappe.log_error>(type(amount), "type")
+		# ~ if amount['alternative'] == 0 and amount['optional'] == 0:
+			# ~ real_ingredients.append(real_ingredients)
+	# ~ return len(real_ingredients)
+	return 7
+
+def data():
+	    matching_projects = frappe.db.sql("""
+        SELECT 
+            `name`, 
+            `drilling_team`, 
+            `expected_start_date`, 
+            `expected_end_date`, 
+            `start_half_day`, 
+            `end_half_day`, 
+            `object`
+        FROM `tabProject`
+        WHERE `project_type` = "External"
+          AND `status` IN ("Open", "Completed")
+          AND 
+            ((`expected_start_date` BETWEEN '{from_date}' AND '{to_date}')
+             OR (`expected_end_date` BETWEEN '{from_date}' AND '{to_date}')
+             OR (`expected_start_date` < '{from_date}' AND `expected_end_date` > '{to_date}')
+            )
+          {customer_filter}
+          {drilling_team_filter}
+        ORDER BY
+            `tabProject`.`expected_start_date` ASC;
+        """.format(from_date=from_date, to_date=to_date, customer_filter=customer_filter, drilling_team_filter=drilling_team_filter), as_dict=True)
+
+	return

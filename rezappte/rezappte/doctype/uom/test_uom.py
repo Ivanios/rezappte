df6bfb674557d6f16430968a2abd0073e9b02ea7
Ivanios <ivan.lochbihler@libracore.com>
2024-07-21 17:36:00 +0200

    added uom and made qty float

---
+++ rezappte/rezappte/doctype/uom/test_uom.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024, Ivanios and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class Testuom(unittest.TestCase):
+	pass

b97db9a19e125a0dcbb3d8c32aeeed9cd55c8c93
Ivanios <ivan.lochbihler@libracore.com>
2024-08-10 23:12:48 +0200

    calcualte ingredietns

--- rezappte/rezappte/doctype/shopping_list/shopping_list.py
+++ rezappte/rezappte/doctype/shopping_list/shopping_list.py
@@ -14,6 +14,7 @@ import json
 def get_shopping_list_html(recipe_list):
 	ingredients = get_ingredients(recipe_list)
 	frappe.log_error(ingredients, "ingredients")
+	calced_ingredients = calc_ingredients(ingredients)
 	return
 	
 def get_ingredients(recipe_list):
@@ -41,4 +42,32 @@ def get_ingredients(recipe_list):
 								WHERE
 									`tabRezept`.`name` IN ({})""".format(formatted_recipe_list), as_dict=True)
 	return ingredients
+	
+def calc_ingredients(ingredients):
+	calced_ingredients = []
+	for ingredient in ingredients:
+		ingredient_uom = frappe.get_value("Zutaten", ingredient.get('ingredient'), "uom")
+		if ingredient.get('uom') != ingredient_uom:
+			actual_ingredient, real_amount, main_uom  = convert_uom(ingredient.get('ingredient'), ingredient.get('amount'), ingredient.get('uom'))
+		else:
+			actual_ingredient, real_amount, main_uom  = ingredient.get('ingredient'), ingredient.get('amount'), ingredient.get('uom')
+		
+		found_match = False
+		for existing_ingredient in calced_ingredients:
+			if existing_ingredient.get('ingredient') == actual_ingredient:
+				found_match = True
+				existing_ingredient['amount'] += real_amount
+		if not found_match:
+			calced_ingredients.append({'ingredient': actual_ingredient, 'amount': real_amount or 0, 'uom': main_uom, 'abteilung': ingredient.get('abteilung')})
+
+	return calced_ingredients
 									
+def convert_uom(ingredient, amount, uom):
+	ingredient_doc = frappe.get_doc("Zutaten", ingredient)
+	main_uom = ingredient_doc.get('uom')
+	for sub_uom in ingredient_doc.get('conversions'):
+		if sub_uom.get('conversion_uom') == uom:
+			real_amount = amount * sub_uom.get('conversion_factor')
+			
+	return ingredient, real_amount, main_uom
+	

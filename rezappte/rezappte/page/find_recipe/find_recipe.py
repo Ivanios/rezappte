90e13904c9df6b9bfdb0f0bf6c91f3bd7bd6783e
Ivanios <ivan.lochbihler@libracore.com>
2024-09-01 21:41:52 +0200

    method to get recipes by ingredients

--- rezappte/rezappte/page/find_recipe/find_recipe.py
+++ rezappte/rezappte/page/find_recipe/find_recipe.py
@@ -3,6 +3,7 @@
 # For license information, please see license.txt
 
 import frappe
+from collections import Counter
 
 @frappe.whitelist()
 def get_colors():
@@ -16,3 +17,33 @@ def get_colors():
         font_color = frappe.db.get_value("Color Scheme", user_card.get('color_scheme'), "font_color")
         
     return {'background_color': background_color, 'font_color': font_color}
+    
+def find_recipe_by_ingredients(ingredients):
+    print(ingredients)
+    # build conditions string
+    conditions = " OR ".join(
+        "`tabRezept Zutaten`.`ingredient` = '{0}'".format(condition.replace("'", "''")) for condition in ingredients
+    )
+    print(conditions)
+    #get al results with at least one hit
+    all_results = frappe.db.sql("""
+                                SELECT
+                                    `tabRezept`.`name`
+                                FROM
+                                    `tabRezept`
+                                LEFT JOIN
+                                    `tabRezept Zutaten` ON `tabRezept`.`name` = `tabRezept Zutaten`.`parent`
+                                WHERE
+                                    {conditions}""".format(conditions=conditions), as_dict=True)
+    
+    all_results_list = []
+    for result in all_results:
+        all_results_list.append(result.get('name'))
+    
+    #count how many searched ingredients are included in each recipe
+    counter = Counter(all_results_list)
+    
+    #sort recipes by most ingredient hits and cut i to 10 recipes
+    list_of_top_recipes = [{'recipe': item, 'count': count} for item, count in counter.most_common(10)]
+    
+    return list_of_top_recipes
